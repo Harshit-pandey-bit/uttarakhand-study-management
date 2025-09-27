73a199d29262d4f8e3b43af8a9bd285cd922788e
Ritik Joshi <155967780+OFF-rtk@users.noreply.github.com>
2025-09-27 12:10:05 +0530

    Delete client directory

--- client/src/components/student/dashboard/progress-ring.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Progress } from '../../ui/progress'
-
-export const ProgressRing: React.FC<{ value: number }> = ({ value }) => (
-  <div className="flex flex-col items-center">
-    <div className="font-semibold text-sm mb-2">Learning Progress</div>
-    <Progress value={value} max={100} showValue variant="circular" />
-  </div>
-)
